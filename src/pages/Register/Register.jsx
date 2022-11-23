884f0e944bc778c746072c1efbc9136e3cbf295d
shihab-sabbir <shihab11231@gmail.com>
2022-11-24 00:09:15 +0600

    authentication done

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -26,10 +26,10 @@ function Register() {
     const userRole = (user) => {
         axios.post('http://localhost:5000/login', user).then((response) => console.log(response))
     }
-    const jwtToken = (user) => {
+    const jwtToken = (user, role) => {
         setLoading(true);
         const uid = user?.uid;
-        fetch(' https://assignment-11-five.vercel.app/jwt', {
+        fetch('http://localhost:5000/jwt', {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
@@ -37,9 +37,10 @@ function Register() {
             body: JSON.stringify({ uid }),
         }).then(res => res.json()).then(data => {
             if (data.token) {
-                localStorage.setItem('assignment-11_Token', data.token)
+                localStorage.setItem('your-car-token', data.token)
                 setLoading(false);
                 setUser(user);
+                user['role'] = role;
                 userRole(user);
                 toast.success('Successfully Login');
                 navigate(from, { replace: true });
@@ -48,7 +49,7 @@ function Register() {
                 toast.error('Login Failed');
                 setLoading(false);
             }
-        })
+        }).catch(err => { console.log(err); setLoading(false); })
     }
     const handleFormSubmit = (e) => {
         setLoading(true); // new added , maybe needed to be removed
@@ -59,15 +60,15 @@ function Register() {
         const formData = new FormData;
         const email = e.email;
         const password = e.password;
+        const role = e.role;
         formData.append('image', e.photo[0]);
         fetch(`https://api.imgbb.com/1/upload?key=${process.env.REACT_APP_Imgbb_Key}`, {
             method: 'POST',
             body: formData
         }).then(res => res.json()).then(data => {
-            console.log(data);
             createUserWithEmailAndPassword(auth, email, password)
                 .then((userCredential) => {
-                    jwtToken(auth.currentUser)
+                    jwtToken(auth.currentUser, role)
                     updateProfile(auth.currentUser, {
                         displayName: e.username,
                         photoURL: data.data.display_url
@@ -119,33 +120,33 @@ function Register() {
             <Helmet>
                 <title>Smile - Register</title>
             </Helmet>
-            <div className="w-full h-fit max-w-md p-8 space-y-3 mb-5 text-slate-800 bg-transparent dark:text-gray-100 border shadow-lg dark:bg-gray-800 dark:border-none">
+            <div className="w-full h-fit max-w-md p-8 space-y-3 mb-5 text-slate-800 bg-transparent dark:text-gray-100  border shadow-lg dark:bg-gray-800 dark:border-none">
                 <h1 className="text-2xl text-center font-thin">Registration Form</h1>
-                <form className="space-y-6 ng-untouched ng-pristine ng-valid" onSubmit={handleSubmit(handleFormSubmit)}>
+                <form className="space-y-6" onSubmit={handleSubmit(handleFormSubmit)}>
                     <div className="space-y-1 text-sm">
-                        <label htmlFor="username" className="block dark:text-gray-400">Username</label>
-                        <input type="text" {...register("username", { required: "Name is required" })} placeholder="Username" className="w-full px-4 py-2  dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
+                        <label htmlFor="username" className="block dark:text-gray-400 text-slate-800">Username</label>
+                        <input type="text" {...register("username", { required: "Name is required" })} placeholder="Username" className="w-full px-4 py-2  dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 text-slate-800 focus:dark:border-violet-400" />
                         {errors.username && <p className='text-red-600' >{errors.username?.message}</p>}
                     </div>
                     <div className="space-y-1 text-sm">
-                        <label htmlFor="Email" className="block dark:text-gray-400">Email</label>
-                        <input type="email" {...register("email", { required: "Email is required" })} placeholder="Email" className="w-full px-4 py-2  dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
+                        <label htmlFor="Email" className="block dark:text-gray-400 text-slate-800">Email</label>
+                        <input type="email" {...register("email", { required: "Email is required" })} placeholder="Email" className="w-full px-4 py-2  dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 text-slate-800 focus:dark:border-violet-400" />
                         {errors.email && <p className='text-red-600' >{errors.email?.message}</p>}
                     </div>
 
                     <div className="space-y-1 text-sm">
-                        <div class="flex flex-col items-center justify-center w-full">
+                        <div className="flex flex-col items-center justify-center w-full">
                             {previewImage !== null && <div className='relative'>
                                 <PhotoView src={previewImage}>
                                     <img src={previewImage} className='!w-[110px] !h-[100px]cursor-zoom-in border-2 p-1' aria-disabled alt="" />
                                 </PhotoView>
                                 <p className='absolute top-[-8px] right-[-15px] rounded-full px-1 text-red-600 cursor-pointer text-xl' onClick={() => { resetField('photo'); previewImage = null }}><ImCross /></p>
                             </div>}
-                            {previewImage === null && <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                                <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                                    <svg aria-hidden="true" class="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
-                                    <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload user image</span></p>
-                                    <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF</p>
+                            {previewImage === null && <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-[120px] border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                                <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                                    <svg aria-hidden="true" className="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
+                                    <p className="mb-2 text-sm dark:text-gray-400 text-slate-800"><span className="font-semibold">Click to upload user image</span></p>
+                                    <p className="text-xs dark:text-gray-400 text-slate-800">SVG, PNG, JPG or GIF</p>
                                 </div>
                                 <input id="dropzone-file" type="file" accept='image/*' {...register("photo", { required: "Photo is required" })} className="hidden" />
                             </label>}
@@ -153,15 +154,22 @@ function Register() {
                         </div>
                     </div>
                     <div className="space-y-1 text-sm">
-                        <label htmlFor="password" className="block dark:text-gray-400">Password</label>
-                        <input type="password" {...register("password", { required: "Password is required" })} placeholder="Password" className="w-full px-4 py-2  dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
+                        <label htmlFor="role" className="block dark:text-gray-400 text-slate-800">Register for ?</label>
+                        <select className="select-bordered w-full text-sm" {...register("role")}>
+                            <option defaultChecked value='buyer'>Buyer</option>
+                            <option value='seller'>Seller</option>
+                        </select>
+                    </div>
+                    <div className="space-y-1 text-sm">
+                        <label htmlFor="password" className="block dark:text-gray-400 text-slate-800">Password</label>
+                        <input type="password" {...register("password", { required: "Password is required" })} placeholder="Password" className="w-full px-4 py-2  dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 text-slate-800 focus:dark:border-violet-400" />
                         {errors.password && <p className='text-red-600' >{errors.password?.message}</p>}
                     </div>
                     <button className="block w-full p-3 text-center rounded-sm 
                     bg-[#00ACBD]
                     text-white bg-gradient-to-l" type='submit'>Sign up</button>
                 </form>
-                <p className="text-xs text-center sm:px-6 dark:text-gray-400">Already have an account?
+                <p className="text-xs text-center sm:px-6 dark:text-gray-400 text-slate-800">Already have an account?
                     <Link rel="noopener noreferrer" to='/login' className="underline dark:text-gray-100 text-slate-800 px-2 text-sm font-bold">Log in</Link>
                 </p>
             </div>
